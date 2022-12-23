53e3196be3f3afb8bf40d38b962c0a6a40f48a5e
ohnishant <nishant.mishra550@gmail.com>
2022-12-23 12:29:04 +0530

    remove: bad nerdtree remap

--- .config/nvim/after/plugin/nerdtree.lua
+++ .config/nvim/after/plugin/nerdtree.lua
@@ -2,5 +2,4 @@ vim.keymap.set("n", "<leader>n", "<Esc>:NERDTreeFocus<CR>")
 vim.keymap.set("n", "<C-n>", "<Esc>:NERDTreeToggle<CR>")
 vim.keymap.set("n", "<leader>ff", "<Esc>:NERDTreeFind<CR>")
 
-vim.keymap.set("i", "<leader>n", "<Esc>:NERDTreeFocus<CR>")
 vim.keymap.set("i", "<C-n>", "<Esc>:NERDTreeToggle<CR>")
