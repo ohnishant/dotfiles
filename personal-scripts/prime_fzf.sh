cb0254c688bb0ef804dd6ac6e647303b11df4534
OhNishant <nishant.mishra550@gmail.com>
2023-06-30 20:20:54 +0530

    sessionizer: edit file paths

--- personal-scripts/prime_fzf.sh
+++ personal-scripts/prime_fzf.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Projects/ ~/college/ ~/Tutorials/ ~/college/100-Days-of-Code/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/dotfiles/.config/ ~/Projects/ ~/college/ ~/Tutorials/  -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
