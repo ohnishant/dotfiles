f4b35ae65a3a05f69648026ea1e853a285752bd1
OhNishant <nishant.mishra550@gmail.com>
2023-11-14 10:40:35 +0530

    nvim: formatting

---
+++ .config/nvim/lua/nish/plugins/lsp/formatting.lua
@@ -0,0 +1,42 @@
+return {
+	"stevearc/conform.nvim",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd", "prettier" },
+				typescript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "black", "isort" },
+				rust = { "rustfmt" },
+				cpp = { "clang-format" },
+				c = { "clang-format" },
+				go = { "gofmt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = true,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mf", function()
+			conform.format({
+				lsp_fallback = true,
+				async = true,
+				timeout_ms = 500,
+			})
+		end)
+	end,
+}
