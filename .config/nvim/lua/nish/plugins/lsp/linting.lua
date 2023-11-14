9c0f1678aa06c6c1c240cf612ad481fbc2e7f9f4
OhNishant <nishant.mishra550@gmail.com>
2023-11-14 10:40:20 +0530

    nvim: linting

---
+++ .config/nvim/lua/nish/plugins/lsp/linting.lua
@@ -0,0 +1,40 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			css = { "stylelint" },
+			html = { "stylelint" },
+			json = { "jsonlint" },
+			markdown = { "markdownlint" },
+			lua = { "luacheck" },
+			python = { "ruff" },
+			rust = { "cargo" },
+			cpp = { "clangtidy" },
+			c = { "clangtidy" },
+			go = { "golangcilint" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		-- Make Linted Keymap
+		vim.keymap.set("n", "<leader>ml", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
+	end,
+}
