66fad49124cb889926b83dbec8bd053ee263a1ba
OhNishant <nishant.mishra550@gmail.com>
2023-11-14 09:36:21 +0530

    nvim: remove the jank from lualine

--- .config/nvim/lua/nish/plugins/lualine.lua
+++ .config/nvim/lua/nish/plugins/lualine.lua
@@ -10,8 +10,10 @@ return {
             options = {
                 icons_enabled = true,
                 theme = 'auto',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
+                section_separators = { left = '', right = ''},
+                component_separators = { left = '|', right = '|'},
+                -- section_separators = { left = '', right = ''},
+                -- component_separators = { left = '', right = ''},
                 disabled_filetypes = {
                     statusline = {},
                     winbar = {},
@@ -29,7 +31,7 @@ return {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
                 lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_x = { "nvim-lsp" ,'encoding', 'fileformat', 'filetype' },
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
             },
